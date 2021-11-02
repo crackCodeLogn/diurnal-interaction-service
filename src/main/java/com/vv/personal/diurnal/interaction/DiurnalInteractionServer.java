51cc6a800eeee6dc8c4375f980d8697da84fd1f7
Vivek <vivektheintel@gmail.com>
2021-11-03 01:47:39 +0530

    Restoring startup and adding shutdown logs and cleaning spring out

--- src/main/java/com/vv/personal/diurnal/interaction/DiurnalInteractionServer.java
+++ src/main/java/com/vv/personal/diurnal/interaction/DiurnalInteractionServer.java
@@ -1,11 +1,15 @@
 package com.vv.personal.diurnal.interaction;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.event.Observes;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Slf4j
 @QuarkusMain
@@ -13,26 +17,35 @@ public class DiurnalInteractionServer {
     private static final String HEROKU_SWAGGER_UI_URL = "https://%s/swagger-ui/index.html";
     private static final String SWAGGER_UI_URL = "http://%s:%s/swagger-ui/index.html";
     private static final String LOCALHOST = "localhost";
-    private static final String LOCAL_SPRING_PORT = "local.server.port";
-    private static final String SPRING_APPLICATION_HEROKU = "spring.application.heroku";
+    private static final String QUARKUS_PORT = "quarkus.http.port";
+    private static final String APP_NAME = "app.name";
+    private static final String APP_HEROKU = "app.heroku";
+
+    @ConfigProperty(name = QUARKUS_PORT)
+    Integer port;
+    @ConfigProperty(name = APP_NAME)
+    String appName;
+    @ConfigProperty(name = APP_HEROKU)
+    String herokuHost;
 
     public static void main(String[] args) {
         Quarkus.run(args);
     }
 
     void onStart(@Observes StartupEvent startupEvent) {
-        log.info("********* Starting *********");
-/*        String host = LOCALHOST;
+        log.info("********* Starting '{}' *********", appName);
+        String host = LOCALHOST;
         try {
             host = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
             log.error("Failed to obtain ip address. ", e);
         }
-        String port = environment.getProperty(LOCAL_SPRING_PORT);
-        String herokuHost = environment.getProperty(SPRING_APPLICATION_HEROKU);
-        log.info("'{}' activation is complete! Expected Heroku Swagger running on: {}, exact url: {}",
-                environment.getProperty("spring.application.name"),
+        log.info("Expected Heroku Swagger running on: {}, exact url: {}",
                 String.format(HEROKU_SWAGGER_UI_URL, herokuHost),
-                String.format(SWAGGER_UI_URL, host, port));*/
+                String.format(SWAGGER_UI_URL, host, port));
+    }
+
+    void onStop(@Observes ShutdownEvent shutdownEvent) {
+        log.info("********* Shutting down '{}' *********", appName);
     }
 }
